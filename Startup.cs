b71c41b86d7c0bd1a574346767c772b105fbebe4
Mutasim Malik <malikmutasim2121@gmail.com>
2021-03-18 18:44:42 +0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace net5Middleware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<justValidator>();
+
+            app.Use(async (context, next) =>
+            {
+                DateTime startTime = DateTime.Now;
+
+                await next();
+
+                DateTime endTime = DateTime.Now;
+
+                TimeSpan processTime = endTime - startTime;
+
+                await context.Response.WriteAsync($"Total Process Time {processTime}");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ justValidator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace net5Middleware
+{
+    public class justValidator
+    {
+        public RequestDelegate _next { get; }
+
+        public justValidator(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            StringValues authorizationToken;
+
+            context.Request.Headers.TryGetValue("x-id", out authorizationToken);
+
+            if (authorizationToken.Count > 0 && authorizationToken[0] == "confusing21")
+                await _next(context);
+            else
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync("Invalid header token");
+
+            return;
+        }
+    }
+}
